2b378c62b45741c4b4cce084f1e49d1615629c70
merinids <mail.hicham@gmail.com>
2024-02-03 15:06:43 -0800

    Update main.js

--- main.js
+++ main.js
@@ -217,7 +217,7 @@ $(function() {
             var action = determineAction(cards, dealerUpcard, total, soft, isPair, busted, isBlackjack);
 
             if(cards.length == 0) {
-                $('#handValue').html("Amber AI loaded. Play Blackjack ✨");
+                $('#handValue').html("Amber AI Blackjack ✨");
             } else if(busted) {
                 // console.log("Hand Value: Bust");
                 $('#handValue').html("Bust (" + total + "). You lose.");
